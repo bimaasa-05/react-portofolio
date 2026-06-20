c3a54c9c0d32da77c47131b0b94d5d28d9b98fc2
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-06-20 16:21:35 +0700

    pesan aja si

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -66,7 +66,7 @@ function Hero() {
 											<img
 												src={profileImg}
 												alt="Foto Profil Cahya Rizqon"
-												className="profile-img"
+												className="profile.jpeg"
 											/>
 										</div>
 									</div>
